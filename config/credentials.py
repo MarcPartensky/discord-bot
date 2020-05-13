f2841669fe291a2e21b9ce6210cd00dbd3f9caea
Marc Partensky <marc.partensky@gmail.com>
2020-05-13 18:45:12 +0200

    Update credentials.py

--- config/credentials.py
+++ config/credentials.py
@@ -1,9 +1,8 @@
-token = "NzAzMzQ3MzQ5NjIzMTQ0NTQ5.XqNpcg.qybAmE4p2XcMC5MnwAgvx3llHSY"
-client_id = 703347349623144549
-# mongo_url = "mongodb+srv://esclave:marcgptl44@discord-cluster-5ckni.mongodb.net/test"
-mongo_url = "mongodb+srv://<username>:<password>@discord-cluster-5ckni.mongodb.net/test"
-mongo_password = "esclave"
-mongo_username = "esclave"
+token = "Your token"
+client_id = "Your id (integer)"
+mongo_url = "mongodb+srv://<username>:<password><your mongo cluseter>/test"
+mongo_password = "mongo_password"
+mongo_username = "mongo_username"
 mongo_url = mongo_url.replace('<username>', mongo_username)
 mongo_url = mongo_url.replace('<password>', mongo_password)
 # mongo_url = mongo_url.replace('mongodb+srv://', 'mongodb://')
