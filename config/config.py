07a8a443b9fe2196407fce1b59961b60f4a86d53
Marc Partensky <marc.partensky@gmail.com>
2020-05-17 03:07:20 +0200

    credentials set correctly

--- config/config.py
+++ config/config.py
@@ -1,19 +1,19 @@
+from .credentials import mongo_url
+
 from utils import access
 import itertools
 
-from .credentials import mongo_url
 from pymongo import MongoClient
 import pymongo
+import os
 
 cluster = MongoClient(mongo_url)
 
-# prefixes = ["esclave", "."]
-prefix = "?"
-# masters = ["Marc Partensky#5983"]#, "Mazex#3106"]
+link = "https://discordapp.com/oauth2/authorize?&client_id=703347349623144549&scope=bot&permissions=8"
+prefix = os.environ['DISCORD_PREFIX']
 masters = [478552571510915072]#, 219949869220102147]
 
 roles = ["@Maître", "@Admin"]
-# bots = ["@Dank Memer#5192"]
 
 delete_after_time = 10
 status = itertools.cycle(["développer la science infuse", "évoluer au delà de l'espèce humaine", "comploter contre l'humanité", "finaliser la théorie quantique"])
--- config/credentials.py
+++ config/credentials.py
@@ -1,17 +1,6 @@
-# token = "NzAzMzQ3MzQ5NjIzMTQ0NTQ5.Xrwq_Q.dinlotDWkH9vBxAkD65lkeqm7Z8" #production
 import os
-print(os.environ)
 token = os.environ['DISCORD_TOKEN']
 client_id = os.environ['DISCORD_CLIENT_ID']
-
-# token = "NzEwODYzNDIzMzA2NTk2MzU0.Xr6qpA.rCYXHsDmGf6rVgz2bQ0yV9oIlO0"
-# client_id = 703347349623144549 #production
-# client_id = 710863423306596354
-# mongo_url = "mongodb+srv://esclave:marcgptl44@discord-cluster-5ckni.mongodb.net/test"
-
-# mongo_password = "esclave"
-# mongo_username = "esclave"
-
 
 mongo_username = os.environ['DISCORD_MONGO_USERNAME']
 mongo_password = os.environ['DISCORD_MONGO_PASSWORD']
@@ -21,6 +10,3 @@ mongo_url = "mongodb+srv://<username>:<password>@<cluster>"
 mongo_url = mongo_url.replace('<username>', mongo_username)
 mongo_url = mongo_url.replace('<password>', mongo_password)
 mongo_url = mongo_url.replace('<cluster>', mongo_cluster)
-# mongo_url = mongo_url.replace('mongodb+srv://', 'mongodb://')
-
-# https://discordapp.com/oauth2/authorize?&client_id=703347349623144549&scope=bot&permissions=8
