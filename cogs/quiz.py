696bcbd89e531a5836bfe01939efd260fa7a0a6a
Marc Partensky <marc.partensky@gmail.com>
2020-07-23 18:30:20 +0200

    quiz

---
+++ cogs/quiz.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+import discord
+
+class Quiz(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+        # self.quiz = quiz.Quiz()
+
+    @commands.group(aliases=['qz'])
+    async def quiz(self, ctx:commands.Context):
+        """Groupe de commandes des Quizs."""
+        if not ctx.invoked_subcommand:
+            await ctx.send(
+                f"> Erreur: commande inexistante."
+                f"\n> Tapez `{self.bot.command_prefix}help quiz` pour voir les commandes disponibles.")
+
+    @quiz.command(name="ajouter", aliases=['add', 'a'])
+    async def add(self, ctx:commands.Context, *, question:str):
+        """Ajoute une question au quiz sélectionné."""
+        question = self.quiz.parse(question)
+        self.quiz.add(question)
+        await ctx.send(f"> Question *{question}* ajoutée par **{ctx.author.name}**.")
+
+    @quiz.command(name="suivant", aliases=['next', 'sv', 'n'])
+    async def next(self, ctx:commands.Context):
+        """Passe à la question suivante."""
+        await self.quiz.next(ctx)
+
+def setup(bot):
+    bot.add_cog(Quiz(bot))
+
