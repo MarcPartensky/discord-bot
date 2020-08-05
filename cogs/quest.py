455da6805db618311a99270903585c13db2775f4
Marc Partensky <marc.partensky@gmail.com>
2020-08-05 20:48:04 +0200

    started quest cog

---
+++ cogs/quest.py
@@ -0,0 +1,45 @@
+from config.config import cluster, access
+
+from discord.ext import commands
+import discord
+
+class Quest(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        """Créer la catégorie des quêtes pour convertir l'énergie en argent."""
+        self.bot = bot
+
+    @commands.command(name="quêtes", aliases=['quests'])
+    async def quests(self, ctx:commands.Context):
+        """Affiche les quêtes disponibles."""
+
+
+    @commands.group(name="quête", aliases=['quest'])
+    async def quest(self, ctx:commands.Context):
+        """Groupe de commande des quêtes."""
+        if not ctx.invoked_subcommand:
+            pass
+
+    @quest.command(aliases=['i'])
+    async def info(self, ctx:commands.Context):
+        """Affiche des informations sur une quête."""
+        pass
+
+    @quest.command(name="accepter", aliases=['accept', 'do'])
+    async def accept(self, ctx:commands.Context):
+        """Accepte une quête."""
+        pass
+
+    @access.admin
+    @quest.command(name="ajouter", aliases=['add', 'a', '+'])
+    async def add(self, ctx:commands.Context):
+        """Ajoute un quête."""
+        pass
+
+    @access.admin
+    @quest.command(name="retirer", aliases=['remove', 'delete', 'd', '-'])
+    async def remove(self, ctx:commands.Context):
+        """Retire une quête."""
+
+
+def setup(bot:commands.Bot):
+    bot.add_cog(Quest(bot))
