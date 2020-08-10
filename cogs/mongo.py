a01f8feab142f3e8479798e9e67e1dfd70b19eea
Marc Partensky <marc.partensky@gmail.com>
2020-08-10 14:47:46 +0200

    updated mongo cog and cleaned the tools

--- cogs/mongo.py
+++ cogs/mongo.py
@@ -1,73 +1,106 @@
 import discord
 
-from discord.ext import commands, tasks
+from discord.ext import commands
 from models.mongo import Post, MongoCollection, MongoDatabase
 
 from config.config import cluster, access
-from utils.tools import for_all_cog_methods, not_invoked_command
+from utils.tools import not_invoked_command, lazy_embed
 
 # @for_all_cog_methods(access.cog_admin)
+
+
 class Mongo(commands.Cog):
     def __init__(self, bot: commands.Bot):
         """Initialize the mongo cog using the bot."""
         self.bot = bot
-        self.database:MongoDatabase = None
-        self.collection:MongoCollection = None
+        self.database: MongoDatabase = None
+        self.collection: MongoCollection = None
         self.post_color = discord.Color.dark_green()
-        
+
     @commands.group(aliases=['mg'])
     async def mongo(self, ctx: commands.Context):
         """Groupe de commandes pour gérer un cluster mongo."""
         if not ctx.invoked_subcommand:
             return await not_invoked_command(ctx, 'mongo')
-            
-        # await ctx
 
-    @commands.command(name='mg-collections')
-    @access.admin
-    async def list_collection_names(self, ctx:commands.Context):
+    @mongo.command(name='collections', aliases=['cls'])
+    async def list_collection_names(self, ctx: commands.Context):
         """Liste les noms des collections."""
-        names = ', '.join(self.database.list_collection_names())
-        await ctx.send(f"Les collections sont: {names}.")
+        names: list = self.database.list_collection_names()
+        if self.collection:
+            names.remove(self.collection.name)
+            names.insert(0, f"**{names.pop(self.collection.name)}**")
+        text = '\n'.join(map(lambda s: f"> - {s}", names))
+        await ctx.send(f"**Les collections sont**:\n{text}")
 
-    @commands.command(name='mg-setcollection')
+    @mongo.group(aliases=['cl'])
+    async def collection(self, ctx: commands.Context):
+        """Groupe de commandes des collections mongo."""
+        if not ctx.invoked_subcommand:
+            return await not_invoked_command(ctx, 'mongo collection')
+
+    @collection.command(name='choisir', aliases=['='])
     @access.admin
-    async def set_collection(self, ctx:commands.Context, *, name:str=None):
+    async def collection_set(self, ctx: commands.Context, *, name: str = None):
         """Choisi une collection."""
         if name:
             self.collection = self.database[name]
+        await ctx.send(f"> La collection **{self.collection.name}** est sélectionnée.")
+
+    @collection.command(name="supprimer", aliases=[''])
+    @access.admin
+    async def collection_delete(self, ctx: commands.Context, *, name: str = None):
+        """Supprime une collection."""
+        if name:
+            collection = self.database[name]
         else:
-            self.collection = None
-        await ctx.send(f"La collection {self.collection.name} est sélectionnée.")
+            collection = self.collection
+        name = collection.name
+        del collection
+        await ctx.send(f"> La collection **{name}** est supprimée.")
 
-    @commands.command(name='mg-databases')
+    @mongo.command(name='databases', aliases=['dbs'])
     @access.admin
-    async def list_database_names(self, ctx:commands.Context):
+    async def list_database_names(self, ctx: commands.Context):
         """Liste les bases de données."""
-        names = ', '.join(cluster.list_database_names())
-        await ctx.send(f"Les base de données sont: {names}.")
+        names: list = cluster.list_database_names()
+        if self.database:
+            names.remove(self.database.name)
+            names.insert(0, f"**{self.database.name}**")
+        text = '\n'.join(map(lambda s: f"> - {s}", names))
+        await ctx.send(f"> **Les base de données sont**:\n{text}")
 
-    @commands.command(name='mg-setdatabase')
+    @mongo.group(aliases=['db'])
+    async def database(self, ctx: commands.Context):
+        """Groupe de commandes des bases de données mongo."""
+        if not ctx.invoked_subcommand:
+            await not_invoked_command(ctx, 'mongo database')
+
+    @database.command(name='choisir', aliases=['='])
     @access.admin
-    async def set_database(self, ctx:commands.Context, *, name:str=None):
+    async def database_set(self, ctx: commands.Context, *, name: str = None):
         """Choisi une collection."""
         if name:
             self.database = cluster[name]
-        else:
-            self.database = None
-        await ctx.send(f"La base de donnée {self.database.name} est sélectionnée.")
+        await ctx.send(f"> La base de donnée **{self.database.name}** est sélectionnée.")
 
-    @commands.command(name="mg-insert-one")
+    @mongo.group(aliases=['p'])
+    async def post(self, ctx: commands.Context):
+        """Groupe de commandes des posts mongos."""
+        if not ctx.invoked_subcommand:
+            await not_invoked_command(ctx, 'mongo post')
+
+    @post.command(name="insert-one")
     @access.admin
-    async def insert_one(self, ctx:commands.Context, id:str, *, value:str):
+    async def insert_one(self, ctx: commands.Context, id: str, *, value: str):
         """Ajoute des posts dans mongo."""
         post = {id: value}
         self.collection.insert_one(post)
         await ctx.send(f"Le post {id}:{value} est inséré dans {self.collection.name}.")
 
-    @commands.command(name="mg-update-one")
+    @post.command(name="update-one")
     @access.admin
-    async def update_one(self, ctx:commands.Context, id:str, *, value:str):
+    async def update_one(self, ctx: commands.Context, id: str, *, value: str):
         """Mets à jour des posts dans mongo."""
         post = {id: value}
         self.collection.update_one(post)
@@ -84,61 +117,51 @@ class Mongo(commands.Cog):
     #     embed = self.lazy_embed(title, self.post_color, post)
     #     await ctx.send(embed=embed)
 
-    @commands.command(name="mg-delete-one")
+    @post.command(name="delete-one")
     @access.admin
-    async def delete_one(self, ctx:commands.Context, *, conditions:str):
+    async def delete_one(self, ctx: commands.Context, *, conditions: str):
         """Trouve un post dans mongo."""
         conditions = conditions.split(',')
-        conditions = dict([string.strip() for string in condition.strip().split('=')] for condition in conditions)
-        # try:
-        print(conditions)
+        conditions = dict([string.strip() for string in condition.strip().split(
+            '=')] for condition in conditions)
         self.collection.find_and_delete_one(conditions)
-        msg = f"Document supprimé."
-        # except Exception as e:
-        #     print(e)
-        #     msg = f"Document introuvable."
-        await ctx.send(msg)
+        await ctx.send("> Document supprimé.")
 
-    def lazy_embed(self, title, color, d):
-        """Lazy embed a dictionary."""
-        embed = discord.Embed(title=title, color=color)
-        for k,v in d.items():
-            embed.add_field(name=k, value=v)
-        return embed
-
-    @commands.command(name="mg-find-one")
+    @post.command(name="find-one")
     @access.admin
-    async def find_one(self, ctx:commands.Context, *, conditions:str):
+    async def find_one(self, ctx: commands.Context, *, conditions: str):
         """Trouve des posts dans mongo."""
         conditions = conditions.split(',')
-        conditions = dict([string.strip() for string in condition.strip().split('=')] for condition in conditions)
+        conditions = dict([string.strip() for string in condition.strip().split(
+            '=')] for condition in conditions)
         post = self.collection.find_one(conditions)
-        embed = self.lazy_embed(post._id, self.post_color, post)
+        embed = lazy_embed(post._id, self.post_color, post)
         await ctx.send(embed=embed)
 
-    @commands.command(name="mg-find")
+    @post.command(name="find")
     @access.admin
-    async def find(self, ctx:commands.Context, *, conditions:str):
+    async def find(self, ctx: commands.Context, *, conditions: str):
         """Trouve des posts dans mongo."""
         conditions = conditions.split(',')
-        conditions = dict([string.strip() for string in condition.strip().split('=')] for condition in conditions)
+        conditions = dict([string.strip() for string in condition.strip().split(
+            '=')] for condition in conditions)
         for post in self.collection.find(conditions):
             post = Post(post)
-            embed = self.lazy_embed(post._id, self.post_color, post)
+            embed = lazy_embed(post._id, self.post_color, post)
             await ctx.send(embed=embed)
 
-    @commands.command(name="mg-find-all")
+    @post.command(name="find-all")
     @access.admin
-    async def find_all(self, ctx:commands.Context, n:int=10):
+    async def find_all(self, ctx: commands.Context, n: int = 10):
         """Trouve des posts dans mongo."""
         for post in self.collection.find({}, limit=n):
             post = Post(post)
-            embed = self.lazy_embed(post._id, self.post_color, post)
+            embed = lazy_embed(post._id, self.post_color, post)
             await ctx.send(embed=embed)
 
-    @commands.command(name="mg-back")
+    @mongo.command(name="back")
     @access.admin
-    async def back(self, ctx:commands.Context):
+    async def back(self, ctx: commands.Context):
         """Fait un retour en arrière."""
         if self.collection:
             self.collection = None
@@ -148,11 +171,6 @@ class Mongo(commands.Cog):
             return await ctx.send("La base de donnée est désélectionnée.")
         return await ctx.send("Rien n'est sélectionné pour l'instant.")
 
-    # @on_command_error
-    # async def cog_command_error(self, ctx, error):
-    #     """Renvoie une réponse en cas d'erreur."""
-        # await ctx.send(error)
-    
 
 def setup(bot):
     mongo = Mongo(bot)
--- utils/tools.py
+++ utils/tools.py
@@ -1,48 +1,65 @@
-from io import StringIO 
+import signal
+from contextlib import contextmanager
+from io import StringIO
 import sys
 from discord.ext import commands
 import discord
 
+
 def parse(cmd, *args, key="$"):
     """Ajoute des arguments dans une commande."""
     i = 0
-    while key in cmd and i<len(args):
-        cmd = cmd.replace(key,args[i].strip(), 1)
-        i+=1
+    while key in cmd and i < len(args):
+        cmd = cmd.replace(key, args[i].strip(), 1)
+        i += 1
     return cmd
+
 
 class Capturing(list):
     """Capture print output and store it."""
+
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
         return self
+
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
         del self._stringio    # free up some memory
         sys.stdout = self._stdout
 
-def name_to_id(ctx:commands.Context, name):
+
+def name_to_id(ctx: commands.Context, name: str):
     """Convert a name to id."""
     member = discord.utils.get(ctx.guild.members, name=name)
     return member.id
+
 
 def id_to_name(bot, id):
     """Convert an id to a name."""
     user = bot.get_user(id)
     return user.name
 
+
 def tag_to_id(tag):
-    return int(tag.replace('<@!', '').replace('>',''))
+    """Convert a discord tag to a discord id."""
+    return int(tag.replace('<@!', '').replace('>', ''))
+
+
 def id_to_tag(id):
+    """Convert a discord id to a discord tag."""
     return f"<@!{id}>"
 
+
 class DictObject(dict):
+    """Tricky way to make a dictionary usable as a python object."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__dict__ = self
 
+
 def post_passer(func, *passed_args, **passed_kwargs):
+    """Decorator that passes additionnal arguments (after its own) to a function."""
     def decorated(*args, **kwargs):
         args = list(args)
         args.extend(passed_args)
@@ -50,6 +67,7 @@ def post_passer(func, *passed_args, **passed_kwargs):
         kwargs.update(passed_kwargs)
         return func(*args, **kwargs)
     return decorated
+
 
 def for_all_cog_methods(decorator_method):
     def decorate(cls):
@@ -67,24 +85,25 @@ def for_all_cog_methods(decorator_method):
         return cls
     return decorate
 
-def keep(t:dict, l:list):
+
+def keep(t: dict, l: list):
+    """Return the list of items of a dictionary whose keys are in a given list."""
     nt = t.copy()
     results = []
-    for k,v in t.items():
+    for k, v in t.items():
         if isinstance(v, dict):
             results.extend(keep(v, l))
         elif isinstance(v, list):
             for e in v:
                 results.extend(keep(e, l))
         elif k in l:
-            results.append((k,v))
+            results.append((k, v))
     return results
 
-import signal
-from contextlib import contextmanager
 
 @contextmanager
 def time_limit(seconds):
+    """Decorator that gives a time limit to execute a function."""
     def signal_handler(signum, frame):
         raise TimeoutError("Timed out!")
     signal.signal(signal.SIGALRM, signal_handler)
@@ -95,8 +114,35 @@ def time_limit(seconds):
         signal.alarm(0)
 
 
-async def not_invoked_command(ctx: commands.Context, group:str):
+async def not_invoked_command(ctx: commands.Context, group: str):
     return await ctx.send(
         f"> **Erreur**: La commande `{ctx.message.content.replace(ctx.bot.command_prefix, '', 1).strip()}` est inexistante."
         f"\n> Tapez `{ctx.bot.command_prefix}help {group}` pour voir la liste des commandes disponibles."
     )
+
+
+def lazy_embed(
+        fields: dict,
+        title: str = None,
+        description: str = None
+        color: bytes = None,
+        footer: str = None,
+        url : str = None,
+        thumbnail : str = None,
+        image: str = None,
+    ):
+    """Lazy way to make an embed."""
+    embed = discord.Embed(
+        title=title,
+        description=description
+        color=color,
+        url=url)
+    if thumbnail:
+        embed.set_thumbnail(url=thumbnail)
+    if image:
+        embed.set_image(url=image)
+    for k, v in fields = items():
+        embed.add_field(name=k, value=v)
+    if footer:
+        embed.set_footer(text=footer)
+    return embed
