5ba7daadfd90f610223eeef35cf91035c58c66b0
marc <marc.partensky@gmail.com>
2021-03-19 18:41:41 +0000

    added voice to bot the wrong way

--- cogs/ialab.py
+++ cogs/ialab.py
@@ -3,6 +3,7 @@ This cog is meant to interact with the ialab api provided on
 marcpartensky.com/ialab.
 """
 
+import os
 import requests
 import discord
 
@@ -23,12 +24,12 @@ class IALab(commands.Cog):
     @commands.command()
     async def ia(self, ctx: commands.Context, *, message: str):
         """Send a message to the ialab bot."""
-        from bs4 import BeautifulSoup
-
         response = requests.post(self.url, data=dict(message=message))
         print(response)
 
         if response.status_code != 200:
+            from bs4 import BeautifulSoup
+
             soup = BeautifulSoup(response.text, "html.parser")
             embed = discord.Embed(
                 title=soup.title.string, description=soup.p.string, url=self.url
@@ -38,6 +39,46 @@ class IALab(commands.Cog):
         else:
             d = response.json()
             await ctx.send(d["answer"])
+
+    async def say(self, ctx: commands.Context, msg, lang):
+        """Dit un message."""
+        from gtts import gTTS
+
+        tts = gTTS(msg, lang=lang)
+        file = f"tts/{msg}.mp3"
+        tts.save(file)
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(file))
+        if not ctx.voice_client:
+            return await ctx.send("Je ne suis pas dans un salon vocal.")
+
+        def after(e):
+            for file in os.listdir("tts"):
+                if file.endswith("mp3"):
+                    os.remove(os.path.join("tts", file))
+            if e:
+                print(f"Erreur de lecture du fichier audio {file}: {e}")
+
+        ctx.voice_client.play(source, after=after)
+
+    @commands.command()
+    async def tell(self, ctx: commands.Context, *, message: str):
+        """Send a message to the ialab bot."""
+
+        response = requests.post(self.url, data=dict(message=message))
+        print(response)
+
+        if response.status_code != 200:
+            from bs4 import BeautifulSoup
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            embed = discord.Embed(
+                title=soup.title.string, description=soup.p.string, url=self.url
+            )
+            await ctx.send(embed=embed)
+
+        else:
+            d = response.json()
+            await self.say(ctx, d["answer"], "en")
 
 
 def setup(bot):
