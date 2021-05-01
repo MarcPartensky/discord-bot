34fdc8cc42ca1143fc8cc416f82ace8755a5eb60
marc <marc.partensky@gmail.com>
2021-05-01 23:22:31 +0200

    empty junior cog

---
+++ cogs/junior.py
@@ -0,0 +1,25 @@
+"""
+Junior ISEP related commands.
+"""
+
+import os
+import requests
+import discord
+
+from discord.ext import commands, tasks
+from config.config import ialab_bot_url, cluster
+from rich import print
+
+
+class Junior(commands.Cog):
+    """Interact with ialab bot with the commands of this cog."""
+
+    def __init__(self, bot: commands.Bot, **kwargs):
+        """Initialize the ialab cog."""
+        super().__init__(**kwargs)
+        self.bot = bot
+
+
+def setup(bot):
+    """Setup the ialab cog."""
+    bot.add_cog(Junior(bot))
