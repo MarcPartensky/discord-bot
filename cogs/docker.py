e5eeba1f98567d2004fe5adc39a673316d5b1239
marc <marc.partensky@gmail.com>
2021-05-02 05:17:39 +0200

    images light

--- cogs/docker.py
+++ cogs/docker.py
@@ -188,7 +188,7 @@ class Docker(commands.Cog):
             [f"\n* {line.replace('_', '‗')}" for line in lines if line.replace(" ", "")]
         )
         print(text)
-        return await ctx.send(f"```md\n{text}\n```")
+        return await ctx.send(text)
 
 
 def setup(bot: commands.Bot):
