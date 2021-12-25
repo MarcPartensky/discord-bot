01f3e77525723dd509f38e6678d4b3a4939ab46e
Marc Partensky <marc.partensky@gmail.com>
2021-12-25 21:19:59 +0100

    api

--- cogs/api.py
+++ cogs/api.py
@@ -2,13 +2,13 @@
 """HTTP web service to interact with the bot."""
 __author__ = "Marc Partensky"
 
-from config.config import cluster, access, check
-from models.mongo import post
-from utils.check import check
-from utils import tools
+# from config.config import cluster, access, check
+# from utils.check import check
+# from utils import tools
 
 from discord.ext import commands
-import discord
+
+# import discord
 
 import server
 from aiohttp import web
@@ -17,7 +17,7 @@ from aiohttp import web
 class API(commands.Cog):
     """HTTP web service to interact with the bot."""
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.server = server.HTTPServer(
             bot=self.bot,
@@ -40,8 +40,9 @@ class API(commands.Cog):
     @server.add_route(path="/", method="GET", cog="API")
     @server.check(predicate="checker", fail_handler="fail_handler")
     async def home(self, request):
+        # self.bot.mes
         return web.json_response(data={"foo": "bar"}, status=200)
 
 
-def setup(bot):
+def setup(bot: commands.Bot):
     bot.add_cog(API(bot))
