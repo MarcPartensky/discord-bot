f02603bc20982422761d596d7890ae2b75202563
Marc Partensky <marc.partensky@gmail.com>
2022-04-02 17:42:13 +0200

    ssh start

--- cogs/ssh.py
+++ cogs/ssh.py
@@ -2,15 +2,11 @@
 """Control ssh sessions."""
 
 import os
-import sys
 import typing
 import discord
-import pxssh
+from pexpect import pxssh
 from discord.ext import commands
 from discord.ext.commands.core import has_role
-
-from utils.tools import create_role_if_missing, Capturing
-
 
 import pexpect
 
@@ -31,32 +27,26 @@ class Shell(commands.Cog):
 
     @has_role(SHELL_ROLE)
     @commands.command()
-    async def ssh(self, ctx: commands.Context):
+    async def ssh(self, ctx: commands.Context, hostname: str, port: int=0):
         """Run a command in a shell"""
+        username=""
         await create_role_if_missing(ctx, SSH_ROLE, self.color)
         embed = discord.Embed(color=self.color)
         embed.set_thumbnail(url=self.thumbnail)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
         embed.description = self.shell_path
-
-        if ctx.author.id in self.shells:
-            shell = self.shells[ctx.author.id]
-            shell.close()
-            del self.shells[ctx.author.id]
-            embed.title = "Closed shell"
-            await ctx.send(embed=embed)
-        else:
-            embed.title = "Opened shell"
-            await ctx.send(embed=embed)
-            shell = pexpect.spawn(command=self.shell_path, encoding="utf-8")
-            self.shells[ctx.author.id] = shell
+        embed.title = "Opened shell"
+        await ctx.send(embed=embed)
+        shell = pxssh.pxssh()
+        shell.login(hostname, username, password)
+        self.shells[ctx.author.id] = shell
 
     @commands.command(name="shells")
     async def list_shells(self, ctx: commands.Context):
-        """List opened shells."""
+        """List ."""
         if len(self.shells) == 0:
-            return await ctx.send("> No shell opened.")
-        embed = discord.Embed(title="Shells :", color=self.color)
+            return await ctx.send("> No active ssh connexion.")
+        embed = discord.Embed(title="Active ssh connexions:", color=self.color)
         embed.set_thumbnail(url=self.thumbnail)
 
         shell_users = []
@@ -74,17 +64,11 @@ class Shell(commands.Cog):
             return
         if msg.author.id in self.shells:
             shell = self.shells[msg.author.id]
-            print(shell.sendline(msg.content))
-            # output = shell.read()
-            # output = shell.before
-            # print(output)
-            shell.expect(pexpect.EOF)
-            # shell.expect(".*\$ ")
-            output = shell.before
-            # output = "\n".join(output_list)
+            shell.sendline(msg.content)
+            shell.prompt()
+            output = shell.before.decode("utf-8")
             print(output)
-            # await msg.channel.send(output)
-            await msg.channel.send("test")
+            await msg.channel.send(output)
 
     # async def cog_common_error(self, ctx, error):
     #     """Close all shell sessions."""
