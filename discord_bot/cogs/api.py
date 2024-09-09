f6fba10fdb99aadbcf7e9edcec70c2e2294840f7
Marc Partensky <marc.partensky@gmail.com>
2024-09-09 22:33:25 +0200

    Backup files using Discord Storages and Automatically do PostgreSQL Backups every day (#17)
    * send file
    
    * send_file_in_chunks backup backup_all retrieve_file send_file
    
    * automatic postgres backup every hour job

--- discord_bot/cogs/api.py
+++ discord_bot/cogs/api.py
@@ -6,16 +6,21 @@ __author__ = "Marc Partensky"
 # from utils.check import check
 # from utils import tools
 
-import os
+import os, time
 import typing
 import uuid
 import traceback
 import discord
-from discord.ext import commands
+import aiofiles
+import subprocess
+import logging
+from discord.ext import commands, tasks
 
 import server
 from aiohttp import web
 
+MAX_CHUNK_SIZE = 8 * 1024 * 1024  # 8 MB
+SECONDS = 3600
 
 class API(commands.Cog):
     """HTTP web service to interact with the bot."""
@@ -32,20 +37,158 @@ class API(commands.Cog):
         )
         self.bot.loop.create_task(self._start_server())
         self.contexts: typing.Dict[str, commands.Context] = {}
+        self.channel_id = int(os.environ["CHANNEL_ID_NOTIF"])
+        self.backup_job.start()
+        # self.scheduler = AsyncIOScheduler()  # Le scheduler APScheduler
+        # self.scheduler.start()  # Démarrer le scheduler
+        # self.setup_jobs()  # Configurer les jobs récurrents
+
+    def _get_timestamp(self):
+        current_time = time.localtime()
+        milliseconds = int((time.time() % 1) * 1000)
+        timestamp = time.strftime(f"%Y-%m-%d_%H-%M-%S_{milliseconds:03d}", current_time)
+        return timestamp
 
     async def _start_server(self):
         """Task to run the HTTP server."""
         await self.bot.wait_until_ready()
         await self.server.start()
 
-    # async def checker(self, a, request):
-    #     print(a)
-    #     print(request)
-    #     # return request.headers.get("authorization") == "password"
-    #     return True
+    async def _split_file(self, file_path: str, chunk_size: int = MAX_CHUNK_SIZE):
+        """
+        Split a file into chunks of specified size.
+        """
+        chunk_paths = []
+        with open(file_path, 'rb') as file:
+            part_number = 1
+            while True:
+                chunk = file.read(chunk_size)
+                if not chunk:
+                    break
+                tmp = self._get_timestamp()
+                chunk_path = f"/tmp/{os.path.basename(file_path)}.{tmp}.{part_number}"
+                async with aiofiles.open(chunk_path, 'wb') as chunk_file:
+                    await chunk_file.write(chunk)
+                chunk_paths.append(chunk_path)
+                part_number += 1
+        return chunk_paths
 
-    async def fail_handler(self, request):
-        return web.json_response(data={"message": "you are not authorized"}, status=401)
+    async def _send_file_in_chunks(self, file_path: str, file_name: str, channel_id: int = 0):
+        """
+        Send a file to Discord in chunks.
+        """
+        channel_id = channel_id or self.channel_id
+        chunk_paths = await self._split_file(file_path)
+
+        channel = self.bot.get_channel(int(channel_id))
+        if not channel:
+            raise ValueError("Discord channel not found")
+
+        tmp = self._get_timestamp()
+        
+        for i, chunk_path in enumerate(chunk_paths, start=1):
+            chunk_file_name = f"{file_name}.{tmp}.{i}"
+            with open(chunk_path, 'rb') as f:
+                await channel.send(file=discord.File(f, filename=chunk_file_name))
+        
+        return chunk_paths
+
+    async def _clean_files(self, file_paths: list):
+        """Remove the specified files."""
+        for file_path in file_paths:
+            os.remove(file_path)
+
+    async def _execute_pg_command(self, command: list, dump_file_path: str, env: dict):
+        """
+        Execute a PostgreSQL dump command.
+        """
+        try:
+            _ = subprocess.run(command + ["--file", dump_file_path], env=env, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"Backup failed: {e.stderr.decode()}")
+
+    async def _backup_postgres(self, *db_names: str) -> str:
+        """
+        Backup specified PostgreSQL databases or all databases if no arguments are provided.
+        Concatenate the backups into a single file if multiple databases are specified.
+        """
+        pg_user = os.getenv("POSTGRES_USER")
+        pg_password = os.getenv("POSTGRES_PASSWORD")
+        pg_host = os.getenv("POSTGRES_HOST", "localhost")
+        pg_port = os.getenv("POSTGRES_PORT", "5432")
+
+        if not pg_user or not pg_password:
+            raise RuntimeError("PostgreSQL credentials are missing")
+
+        env = os.environ.copy()
+        env["PGPASSWORD"] = pg_password
+
+        filename = "backup.sql"
+
+        if db_names:
+            # Création d'un fichier de dump unique pour plusieurs bases
+            dump_file_path = f"/tmp/{filename}"
+            
+            # Ouvre le fichier pour concaténer les résultats des dumps
+            async with aiofiles.open(dump_file_path, 'w') as dump_file:
+                for db_name in db_names:
+                    pg_dump_command = [
+                        "pg_dump",
+                        "--username", pg_user,
+                        "--host", pg_host,
+                        "--port", pg_port,
+                        "--dbname", f"postgresql://{pg_user}@{pg_host}:{pg_port}/{db_name}"
+                    ]
+                    
+                    try:
+                        # Exécute pg_dump et capture la sortie
+                        result = subprocess.run(pg_dump_command, env=env, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        await dump_file.write(result.stdout.decode())  # Ajoute le dump au fichier
+                    except subprocess.CalledProcessError as e:
+                        raise RuntimeError(f"Backup failed for {db_name}: {e.stderr.decode()}")
+            
+        else:
+            # Si aucune base de données spécifiée, on sauvegarde toutes les bases
+            dump_file_path = f"/tmp/{filename}"
+            pg_dumpall_command = [
+                "pg_dumpall",
+                "--username", pg_user,
+                "--host", pg_host,
+                "--port", pg_port
+            ]
+            
+            try:
+                await self._execute_pg_command(pg_dumpall_command, dump_file_path, env)
+            except Exception as e:
+                raise RuntimeError(f"Backup all databases failed: {str(e)}")
+
+        chunks = await self._send_file_in_chunks(dump_file_path, filename)
+        await self._clean_files(chunks)
+        os.remove(dump_file_path)
+            
+        return dump_file_path
+
+    # def setup_jobs(self):
+    #     """Configurer les jobs récurrents avec APScheduler."""
+    #     # Planifier le job quotidien à 5h du matin
+    #     self.scheduler.add_job(
+    #         self.daily_backup,  # La fonction à exécuter
+    #         CronTrigger(hour=5, minute=0),  # CronTrigger à 5h00 chaque jour
+    #         name="daily_backup_job",  # Nom du job
+    #         replace_existing=True  # Remplacer si le job existe déjà
+    #     )
+    #     logging.info("Job de backup quotidien à 5h du matin configuré.")
+
+    @tasks.loop(seconds=SECONDS)
+    async def backup_job(self):
+        """La tâche qui sera exécutée tous les jours à 5h."""
+        try:
+            # Effectuer un backup de toutes les bases
+            dump_file_paths = await self._backup_postgres()
+            logging.info(f"Backup quotidien réussi : {dump_file_paths}")
+        except Exception as e:
+            logging.error(f"Erreur lors du backup quotidien : {e}")
+
 
     @server.add_route(path="/send/user", method="POST", cog="API")
     async def send_user(self, request: web.Request):
@@ -68,6 +211,160 @@ class API(commands.Cog):
             data={"text": "Successfully sent message."}, status=200
         )
 
+    @server.add_route(path="/send/file", method="POST", cog="API")
+    async def send_file_to_channel(self, request: web.Request):
+        """Endpoint to send a file to a specific channel by ID."""
+        filename: str
+        channel_id: int
+
+        reader = await request.multipart()
+        field = await reader.next()
+        if field.name == "channel_id":
+            channel_id = await field.text()
+        else:
+            return web.json_response({"error": "Channel ID missing"}, status=400)
+
+
+        field = await reader.next()
+        if field.name == "file":
+            filename = field.filename
+            file_path = f"/tmp/{filename}"
+
+            # Save the file temporarily
+            async with aiofiles.open(file_path, 'wb') as f:
+                while True:
+                    chunk = await field.read_chunk()  # 8192 bytes by default
+                    if not chunk:
+                        break
+                    await f.write(chunk)
+        else:
+            return web.json_response({"error": "File missing"}, status=400)
+
+        channel: discord.TextChannel = await self.bot.fetch_channel(channel_id)
+        if not channel:
+            return web.json_response({"error": "Channel not found"}, status=404)
+
+        # await channel.send(file=discord.File(file_path))
+        await self._send_file_in_chunks(file_path, filename, channel_id)
+
+        # Optionally, delete the temp file after sending it
+        os.remove(file_path)
+        message = f"Uploaded {filename} channel: {channel_id}"
+        print(message)
+        return web.json_response({"message": message}, status=200)
+
+
+    @server.add_route(path="/retrieve/file", method="POST", cog="API")
+    async def retrieve_file_from_channel(self, request: web.Request):
+        """
+        Endpoint to retrieve, reassemble, and download the original file from a channel
+        based on its name and the channel ID.
+        """
+        # Extract the JSON data from the request
+        body = await request.json()
+
+        # Extract the required information: channel ID and original filename
+        channel_id = body.get("channel_id") or self.channel_id
+        original_filename = body.get("filename")
+        chosen_timestamp = body.get("timestamp")
+        print('timestamp:', chosen_timestamp)
+
+        if not channel_id or not original_filename:
+            return web.json_response({"error": "Channel ID or filename missing"}, status=400)
+
+        # Fetch the Discord channel
+        channel: discord.TextChannel = await self.bot.fetch_channel(channel_id)
+        if not channel:
+            return web.json_response({"error": "Channel not found"}, status=404)
+
+        # Prepare the path to save the reassembled file
+        reassembled_file_path = f"/tmp/{original_filename}"
+
+        # Initialize list to collect all file chunks
+        file_chunks = []
+
+        stop = False
+        found = False
+
+        def is_well_formed(dfilename: str):
+            if not dfilename.startswith(original_filename):
+                return False
+            dextension = dfilename[len(original_filename)+1:]
+            if dextension.count(".") != 1:
+                return False
+            dtimestamp, dpart = dextension.split(".")
+            # print(f"dtimestamp: {dtimestamp}, {len(dtimestamp)}")
+            # print(f"dpart: {dpart}")
+            if len(dtimestamp) != 23:
+                return False
+            if not dpart:
+                return False
+            # print("passed:", dfilename)
+            return True
+
+        def deconstruct(dfilepath: str):
+            dfilename = dfilepath.split('/')[-1]
+            dextension = dfilename[len(original_filename)+1:]
+            dtimestamp, dpart = dextension.split(".")
+            return dextension, dtimestamp, dpart
+
+
+        # Iterate through the channel's message history to find the file chunks
+        async for message in channel.history(limit=1000):  # Adjust limit as needed
+            if stop and found: break
+            for attachment in message.attachments:
+                print(f"attachement found: {attachment.filename}")
+                dfilename = attachment.filename
+                if not is_well_formed(dfilename):
+                    # print(f"skip and delete {dfilename} because malformed")
+                    await message.delete()
+                    continue
+                if dfilename.startswith(original_filename):
+                    dextension = dfilename[len(original_filename)+1:]
+                    if dextension.count(".") != 1:
+                        break
+                    dtimestamp, dpart = dextension.split(".")
+                    # print("dfilename", dfilename)
+                    # print("dextension", dextension)
+                    # print("dtimestamp", dtimestamp)
+                    # print("dpart", dpart)
+                    # print("chosen_timestamp", chosen_timestamp)
+                    if not chosen_timestamp:
+                        chosen_timestamp = dtimestamp
+                    if chosen_timestamp == dtimestamp:
+                        # print("found", dtimestamp)
+                        found = True
+                    elif found:
+                            # print("stop", chosen_timestamp, "!=", dtimestamp)
+                            stop = True
+                            break
+
+                    chunk_path = f"/tmp/{dfilename}"
+                    await attachment.save(chunk_path)
+                    file_chunks.append(chunk_path)
+
+        if not file_chunks:
+            return web.json_response({"error": "No file chunks found in the channel."}, status=404)
+
+        # Sort the chunks by part number
+        print("sorting chunks", file_chunks)
+        file_chunks.sort(key=lambda filepath: int(deconstruct(filepath)[-1]))
+
+        # Reassemble the file
+        async with aiofiles.open(reassembled_file_path, 'wb') as reassembled_file:
+            for chunk_path in file_chunks:
+                async with aiofiles.open(chunk_path, 'rb') as chunk_file:
+                    data = await chunk_file.read()
+                    await reassembled_file.write(data)
+
+        # Optionally clean up the chunk files after reassembly
+        for chunk_path in file_chunks:
+            os.remove(chunk_path)
+
+        # Serve the reassembled file as a download response
+        headers = {'Content-Disposition': f'attachment; filename="{original_filename}"'}
+        return web.FileResponse(reassembled_file_path, headers=headers)
+
     # def build_context(self, channel: discord.TextChannel, args: list) -> commands.Context:
     #     """Build a fake context to run bot commands."""
     #     # state = discord
@@ -76,6 +373,43 @@ class API(commands.Cog):
     #     message = discord.Message(state, channel, data)
     #     view = discord.channel.TextChannel
     #     return commands.Context(message, self.bot, view, args=args))
+
+
+    @server.add_route(path="/backup", method="POST", cog="API")
+    async def backup_postgres(self, request: web.Request):
+        """
+        Endpoint to create a backup (dump) of a specified PostgreSQL database
+        or all databases if no database name is provided, and return the backup file
+        as a downloadable response.
+        """
+        body = await request.json()
+        db_name = body.get("db_name")
+        dump_file_paths : list
+
+        if db_name and not isinstance(db_name, str):
+            return web.json_response({"error": "db_name must be a string"}, status=400)
+
+        try:
+            if db_name:
+                dump_file_path = await self._backup_postgres(db_name)
+            else:
+                dump_file_path = await self._backup_postgres()
+        except Exception as e:
+            return web.json_response({"error": str(e)}, status=500)
+
+        headers = {'Content-Disposition': f'attachment; filename="{os.path.basename(dump_file_path)}"'}
+        return web.FileResponse(dump_file_path, headers=headers)
+
+    @commands.command(name="backup")
+    async def backup_postgres_command(self, ctx: commands.Context, *db_names: str):
+        """
+        Commande Discord pour créer une sauvegarde (dump) de bases de données PostgreSQL spécifiques
+        ou de toutes les bases de données si aucun nom n'est fourni.
+        """
+        try:
+            await self._backup_postgres(*db_names)
+        except Exception as e:
+            await ctx.send(f"Erreur: {str(e)}")
 
     @server.add_route(path="/command/channel", method="POST", cog="API")
     async def command_channel(self, request: web.Request):
@@ -134,7 +468,6 @@ class API(commands.Cog):
         return web.json_response(data=self.bot, status=200)
 
     @server.add_route(path="/live", method="GET", cog="API")
-    # @server.check(predicate=checker, fail_handler="fail_handler")
     async def live(self, request: web.Request):
         """API home path."""
         return web.json_response(text="OK", status=200)
