4755fd2824a536538efe084cdd1fb54446b8c381
Marc Partensky <marc.partensky@gmail.com>
2024-09-15 22:24:19 +0200

    Dockerfiler with build.sh that works for dockerfile in prod (#22)
    * build
    
    * build.sh
    
    * it works but i dont know why
    
    * dont understand but done trying to

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+apk update
+
+pip install -U uv &
+apk add --no-cache build-base libffi-dev ffmpeg postgresql p7zip &
+wait
+
+uv sync -v
+rm -rf .venv
+uv sync -v
+
+# apk del .build
+# rm -rf /tmp/* /var/apk/cache/*
