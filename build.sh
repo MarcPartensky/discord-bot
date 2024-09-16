019b034612a01442b6d7611c3e57140b3e9703b2
Marc Partensky <marc.partensky@gmail.com>
2024-09-16 16:37:10 +0200

    test with just pip

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ apk add --no-cache --virtual .build build-base libffi-dev &
 wait
 export PATH="$PATH:$HOME/.cargo/bin"
 
-uv sync -v &
+# uv sync -v &
+uv pip compile pyproject.toml -o requirements.txt
+# uv export > requirements.txt
+# sed -i '1,2d' requirements.txt
+pip install -r requirements.txt  &
 apk add ffmpeg postgresql p7zip &
 wait
-
-# pip install -r requirements.txt 
 # apk del .build &
 # rm -rf /var/cache/apk/* /tmp/* &
 # wait
