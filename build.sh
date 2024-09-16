09a59c5b666c1e2ccd5fc17c10733b0ba4da1f7c
Marc Partensky <marc.partensky@gmail.com>
2024-09-16 15:15:20 +0200

    build

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ curl -LsSf https://astral.sh/uv/install.sh | sh &
 apk add --no-cache --virtual .build build-base libffi-dev &
 wait
 
+uv sync -v  &
 apk add ffmpeg postgresql p7zip &
-# pip install -r requirements.txt 
-uv sync -v &
 wait
 
+# pip install -r requirements.txt 
 # apk del .build &
 # rm -rf /var/cache/apk/* /tmp/* &
 # wait
